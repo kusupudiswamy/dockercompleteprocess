0939debb2aff6d3c609b67730b716a8356fffdb9
kusupudiswamy <39132487+kusupudiswamy@users.noreply.github.com>
2018-10-24 19:55:01 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 buildnumber=$1
 exists=$(docker service ls|grep docker)
-if [! "$exists"]; then
+if [ ! "$exists"]; then
 docker service create --name subbu -p 8888:8080 kusupudiswamy/docker:$buildnumber
 else
 docker service update subbu --image kusupudiswamy/docker:$buildnumber
