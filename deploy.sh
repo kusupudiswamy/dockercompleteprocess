56a4c020deb9be835c184cc8bf32ae034349db13
kusupudiswamy <39132487+kusupudiswamy@users.noreply.github.com>
2018-10-24 19:56:31 +0530

    changed port numbers

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 buildnumber=$1
 exists=$(docker service ls|grep docker)
 if [ ! "$exists"]; then
-docker service create --name subbu -p 8888:8080 kusupudiswamy/docker:$buildnumber
+docker service create --name subbu -p 8889:8080 kusupudiswamy/docker:$buildnumber
 else
 docker service update subbu --image kusupudiswamy/docker:$buildnumber
 fi
